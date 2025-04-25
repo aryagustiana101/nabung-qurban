3a5ffd3f298c9a9267694c25a119bfe6905f2f0c
Arya Gustiana <aryagustiana101@gmail.com>
2025-04-26 03:38:47 +0700

    feat: setup shadcn ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "~/components/ui/button";
+import { db } from "~/server/db";
 
-export default function HomePage() {
-  return <Button>Hello world!</Button>;
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const user = await db.user.findFirst();
+
+  return <Button>{user?.name}</Button>;
 }
